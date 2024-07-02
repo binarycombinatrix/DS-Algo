86e13b0765b983ab1314c3da40c12c0e25dea8bd
binarycombinatrix <rpanch163@gmail.com>
2024-07-02 21:07:08 +0530

    Update LCremove-duplicates-from-sorted-array.js

--- LCremove-duplicates-from-sorted-array.js
+++ LCremove-duplicates-from-sorted-array.js
@@ -1,4 +1,4 @@
-/**
+/**                                                 Set approach
  * @param {number[]} nums
  * @return {number}
  */
@@ -11,4 +11,32 @@ var removeDuplicates = function(nums) {
     }
 
     return uniqueArr.length
+};
+
+
+
+
+/**                                                      Two Pointer approach
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+
+    if(nums.length < 2) return nums.length;
+
+    let unique1=0, unique2=1;
+    while(unique2 < nums.length){
+        if(nums[unique1] === nums[unique2]) unique2++
+
+        else{
+            let swap = nums[unique1+1]
+            nums[unique1+1] = nums[unique2]
+            nums[unique2] = swap
+
+            unique1++
+            unique2++
+        }
+    }
+
+    return unique1+1
 };
